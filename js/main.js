02777e090869f32510a4ceca7aa9ef175407d77b
spiderfencer1 <spiderfencer@Abhinavs-MacBook-Pro.local>
2017-09-04 15:15:26 -0700

    next commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 window.onload = () =>
 {
+	function id(i){return document.getElementById(i);}
+	function cls(c){return document.getElementsByClassName(c);}
+	function tag(t){return document.getElementsByTagName(t);}
+
 	function parseLen(text)
 	{
-		return (text < 10) ? '0' + text : text;
+		return (-1 < text && text < 10) ? '0' + text : text;
 	}
 
 	const months =
@@ -12,8 +16,8 @@ window.onload = () =>
 		'Sep','Oct','Nov','Dec'
 	];
 
-	const timeD = document.getElementById('time');
-	const dateD = document.getElementById('date');
+	const timeD = id('time');
+	const dateD = id('date');
 	setInterval(() =>
 	{
 		const year = new Date().getFullYear();
@@ -29,45 +33,45 @@ window.onload = () =>
 
 	function setColor(color)
 	{
-		[].forEach.call(document.getElementsByClassName('clr-bg'),
+		[].forEach.call(cls('clr-bg'),
 			elem => elem.style.backgroundColor = color);
-		[].forEach.call(document.getElementsByClassName('clr-fg'),
+		[].forEach.call(cls('clr-fg'),
 			elem => elem.style.color = color);
-		[].forEach.call(document.getElementsByClassName('prog-bar'),
+		[].forEach.call(cls('prog-bar'),
 			elem => elem.setAttribute('stroke',color))
 	}
 
-	document.getElementById('change-color').onclick = () =>
+	id('change-color').onclick = () =>
 	{
-		setColor(document.getElementById('color-value').value);
+		setColor(id('color-value').value);
 	};
 
-	[].forEach.call(document.getElementsByClassName('color'),
+	[].forEach.call(cls('color'),
 		elem => elem.onclick = () =>
 	{
 		setColor(elem.textContent);
 	});
 
-	[].forEach.call(document.getElementsByClassName('add'),
+	[].forEach.call(cls('add'),
 		e => e.onclick = () =>
 	{
-		const elem = document.getElementsByClassName('percent')[[].indexOf.call(document.getElementsByClassName('add'),e)];
+		const elem = cls('percent')[[].indexOf.call(cls('add'),e)];
 		elem.value = Math.min(parseInt(elem.value) + 1,360);
 	});
 
-	[].forEach.call(document.getElementsByClassName('sub'),
+	[].forEach.call(cls('sub'),
 		e => e.onclick = () =>
 	{
-		const elem = document.getElementsByClassName('percent')[[].indexOf.call(document.getElementsByClassName('sub'),e)];
+		const elem = cls('percent')[[].indexOf.call(cls('sub'),e)];
 		elem.value = Math.max(parseInt(elem.value) - 1,0);
 	});
 
-	[].forEach.call(document.getElementsByClassName('percent'),
+	[].forEach.call(cls('percent'),
 		e => e.onkeydown = evt =>
 	{
 		if(evt.keyCode === 13)
 		{
-			const box = document.getElementsByClassName('progress')[[].indexOf.call(document.getElementsByClassName('percent'),e)];
+			const box = cls('progress')[[].indexOf.call(cls('percent'),e)];
 			box.style.width = e.value + '%';
 			evt.preventDefault();
 		}
@@ -84,7 +88,7 @@ window.onload = () =>
 			{
 				if(results[3])
 				{
-					document.getElementById('city').textContent = results[3].formatted_address;
+					id('city').textContent = results[3].formatted_address;
 				}
 			}
 		});
@@ -103,15 +107,15 @@ window.onload = () =>
 
 	//==================================================================
 
-	[].forEach.call(document.getElementsByClassName('prog-bar'),
+	[].forEach.call(cls('prog-bar'),
 		elem =>
 	{
 		setInterval(() =>
 		{
-			let disp = document.getElementsByClassName('percent')
+			let disp = cls('percent')
 			[[].indexOf.call
 			(
-				document.getElementsByClassName('prog-bar'),
+				cls('prog-bar'),
 				elem
 			)];
 			let prog = Math.min(Math.max(isNaN(parseInt(disp.value)) ? 0 : parseInt(disp.value),0),100);
@@ -136,138 +140,186 @@ window.onload = () =>
 		},1000/100);
 	});
 
-	document.getElementById('save').onclick = () =>
+	id('save').onclick = () =>
 	{
 		const data =
 		{
-			'color':document.getElementsByClassName('clr-fg')[0].style.color || '#fff',
-			'note1':document.getElementById('note1').childNodes[0].value || '',
-			'note2':document.getElementById('note2').childNodes[0].value || '',
+			'color':cls('clr-fg')[0].style.color || '#fff',
+			'note1':id('note1').childNodes[0].value || '',
+			'note2':id('note2').childNodes[0].value || '',
 			'goal0':
 			{
-				'name':document.getElementsByClassName('goal')[0].value || '',
-				'data':document.getElementsByClassName('percent')[0].value || ''
+				'name':cls('goal')[0].value || '',
+				'data':cls('percent')[0].value || ''
 			},
 			'goal1':
 			{
-				'name':document.getElementsByClassName('goal')[1].value || '',
-				'data':document.getElementsByClassName('percent')[1].value || ''
+				'name':cls('goal')[1].value || '',
+				'data':cls('percent')[1].value || ''
 			},
 			'goal2':
 			{
-				'name':document.getElementsByClassName('goal')[2].value || '',
-				'data':document.getElementsByClassName('percent')[2].value || ''
+				'name':cls('goal')[2].value || '',
+				'data':cls('percent')[2].value || ''
 			},
 			'goal3':
 			{
-				'name':document.getElementsByClassName('goal')[3].value || '',
-				'data':document.getElementsByClassName('percent')[3].value || ''
+				'name':cls('goal')[3].value || '',
+				'data':cls('percent')[3].value || ''
 			},
 			'goal4':
 			{
-				'name':document.getElementsByClassName('goal')[4].value || '',
-				'data':document.getElementsByClassName('percent')[4].value || ''
+				'name':cls('goal')[4].value || '',
+				'data':cls('percent')[4].value || ''
 			},
 			'goal5':
 			{
-				'name':document.getElementsByClassName('goal')[5].value || '',
-				'data':document.getElementsByClassName('percent')[5].value || ''
+				'name':cls('goal')[5].value || '',
+				'data':cls('percent')[5].value || ''
 			},
 			'goal6':
 			{
-				'name':document.getElementsByClassName('goal')[6].value || '',
-				'data':document.getElementsByClassName('percent')[6].value || ''
+				'name':cls('goal')[6].value || '',
+				'data':cls('percent')[6].value || ''
 			},
 			'goal7':
 			{
-				'name':document.getElementsByClassName('goal')[7].value || '',
-				'data':document.getElementsByClassName('percent')[7].value || ''
+				'name':cls('goal')[7].value || '',
+				'data':cls('percent')[7].value || ''
 			},
 			'goal8':
 			{
-				'name':document.getElementsByClassName('goal')[8].value || '',
-				'data':document.getElementsByClassName('percent')[8].value || ''
+				'name':cls('goal')[8].value || '',
+				'data':cls('percent')[8].value || ''
 			},
 			'goal9':
 			{
-				'name':document.getElementsByClassName('goal')[9].value || '',
-				'data':document.getElementsByClassName('percent')[9].value || ''
-			}
+				'name':cls('goal')[9].value || '',
+				'data':cls('percent')[9].value || ''
+			},
+			'task0':
+			{
+				'name':cls('task-name')[0].value || '',
+				'time':cls('task-time')[0].value || ''
+			},
+			'task1':
+			{
+				'name':cls('task-name')[1].value || '',
+				'time':cls('task-time')[1].value || ''
+			},
+			'task2':
+			{
+				'name':cls('task-name')[2].value || '',
+				'time':cls('task-time')[2].value || ''
+			},
+			'task3':
+			{
+				'name':cls('task-name')[3].value || '',
+				'time':cls('task-time')[3].value || ''
+			},
+			'task4':
+			{
+				'name':cls('task-name')[4].value || '',
+				'time':cls('task-time')[4].value || ''
+			},
+			'task5':
+			{
+				'name':cls('task-name')[5].value || '',
+				'time':cls('task-time')[5].value || ''
+			},
+			'task6':
+			{
+				'name':cls('task-name')[6].value || '',
+				'time':cls('task-time')[6].value || ''
+			},
+			'task7':
+			{
+				'name':cls('task-name')[7].value || '',
+				'time':cls('task-time')[7].value || ''
+			},
+			'task8':
+			{
+				'name':cls('task-name')[8].value || '',
+				'time':cls('task-time')[8].value || ''
+			},
+			'task9':
+			{
+				'name':cls('task-name')[9].value || '',
+				'time':cls('task-time')[9].value || ''
+			},
 		};
 		const text = JSON.stringify(data);
 		const dl = document.createElement('a');
-		dl.setAttribute('href','data:text/plain;charset=utf-8,' + 
-			encodeURIComponent(text));
+		dl.setAttribute('href','data:text/plain;charset=utf-8,' + encodeURIComponent(text));
 		dl.setAttribute('download','abhi.dashB');
 		dl.style.display = 'none';
 		document.body.appendChild(dl);
 		dl.click();
 		document.body.removeChild(dl);
-		/*
-		localStorage.setItem('dashB-color',data.color);
-		localStorage.setItem('dashB-note1',data.note1);
-		localStorage.setItem('dashB-note2',data.note2);
-		localStorage.setItem('dashB-goal0name',data.goal0.name);
-		localStorage.setItem('dashB-goal0data',data.goal0.data);
-		localStorage.setItem('dashB-goal1name',data.goal1.name);
-		localStorage.setItem('dashB-goal1data',data.goal1.data);
-		localStorage.setItem('dashB-goal2name',data.goal2.name);
-		localStorage.setItem('dashB-goal2data',data.goal2.data);
-		localStorage.setItem('dashB-goal3name',data.goal3.name);
-		localStorage.setItem('dashB-goal3data',data.goal3.data);
-		localStorage.setItem('dashB-goal4name',data.goal4.name);
-		localStorage.setItem('dashB-goal4data',data.goal4.data);
-		localStorage.setItem('dashB-goal5name',data.goal5.name);
-		localStorage.setItem('dashB-goal5data',data.goal5.data);
-		localStorage.setItem('dashB-goal6name',data.goal6.name);
-		localStorage.setItem('dashB-goal6data',data.goal6.data);
-		localStorage.setItem('dashB-goal7name',data.goal7.name);
-		localStorage.setItem('dashB-goal7data',data.goal7.data);
-		localStorage.setItem('dashB-goal8name',data.goal8.name);
-		localStorage.setItem('dashB-goal8data',data.goal8.data);
-		localStorage.setItem('dashB-goal9name',data.goal9.name);
-		localStorage.setItem('dashB-goal9data',data.goal9.data);
-		*/
 	};
 
-	document.getElementById('fileid').onchange = () =>
+	id('fileid').onchange = () =>
 	{
-		const fl = document.getElementById('fileid').files[0];
+		const fl = id('fileid').files[0];
 		const reader = new FileReader();
 		reader.onload = (e) =>
 		{
 			const data = JSON.parse(e.target.result);
 			setColor(data.color);
-			document.getElementById('note1').childNodes[0].value = data.note1;
-			document.getElementById('note2').childNodes[0].value = data.note2;
+			id('note1').childNodes[0].value = data.note1 || '';
+			id('note2').childNodes[0].value = data.note2 || '';
 			for(let i=0;i<10;i++)
 			{
-				document.getElementsByClassName('goal')[i].value = 
-					data['goal' + i].name;
-				document.getElementsByClassName('percent')[i].value = 
-					data['goal' + i].data;
+				cls('goal')[i].value = 
+					data['goal' + i].name || '';
+				cls('percent')[i].value = 
+					data['goal' + i].data || '';
 			}
 		};
 		reader.readAsText(fl);
 	};
 
-	document.getElementById('load').onclick = () =>
+	id('load').onclick = () => {id('fileid').click();};
+
+	const tasks = [0,0,0,0,0,0,0,0,0,0];
+
+	[].forEach.call(cls('task-start'),
+		elem => elem.onclick = () =>
 	{
-		document.getElementById('fileid').click();
-		/*
-		const color = localStorage.getItem('dashB-color');
-		const note1 = localStorage.getItem('dashB-note1');
-		const note2 = localStorage.getItem('dashB-note2');
-		setColor(color);
-		document.getElementById('note1').childNodes[0].value = note1;
-		document.getElementById('note2').childNodes[0].value = note2;
-		for(let i=0;i<10;i++)
+		const idx = [].indexOf.call(cls('task-start'),elem);
+		let i = 0;
+		tasks[idx] = setInterval(() =>
 		{
-			document.getElementsByClassName('goal')[i].value = 
-				localStorage.getItem('dashB-goal' + i + 'name');
-			document.getElementsByClassName('percent')[i].value = 
-				localStorage.getItem('dashB-goal' + i + 'data');
-		}
-		*/
-	};
+			i++;
+			const dt = cls('task-time')[idx].value.split(':').map(v => parseInt(v));
+			let h = (i % 3600 === 0) ? dt[0] - 1 : dt[0];
+			let m = (i % 60   === 0) ? dt[1] - 1 : dt[1];
+			let s = dt[2] - 1;
+			if(s === -1)
+			{
+				m--;
+				s = 59;
+			}
+			if(m === -1)
+			{
+				h--;
+				m = 59;
+			}
+			if(h === -1)
+			{
+				clearInterval(tasks[idx]);
+			}
+			h = parseLen(h);
+			m = parseLen(m);
+			s = parseLen(s);
+			cls('task-time')[idx].value = h + ':' + m + ':' + s;
+		},1000);
+	});
+
+	[].forEach.call(cls('task-stop'),
+		elem => elem.onclick = () =>
+	{
+		const idx = [].indexOf.call(cls('task-stop'),elem);
+		clearInterval(tasks[idx]);
+	});
 };
